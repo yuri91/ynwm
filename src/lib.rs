fe60006c25c35b016c180b96c6ac729f27e69fd5
Yuri Iozzelli <y.iozzelli@gmail.com>
2019-12-01 21:38:39 +0100

    Add more listener stubs

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,14 @@ pub struct Server {
     socket_name: String,
 
     backend_new_output_listener: wl_listener,
+    backend_new_input_listener: wl_listener,
     xdg_shell_new_surface_listener: wl_listener,
+    cursor_motion_listener: wl_listener,
+    cursor_motion_absolute_listener: wl_listener,
+    cursor_button_listener: wl_listener,
+    cursor_axis_listener: wl_listener,
+    cursor_frame_listener: wl_listener,
+    seat_request_set_cursor_listener: wl_listener,
 
     #[allow(dead_code)]
     unpin: PhantomPinned,
@@ -41,8 +48,16 @@ impl Server {
             seat: std::ptr::null_mut(),
             output_layout: std::ptr::null_mut(),
             socket_name: String::new(),
+
             backend_new_output_listener: unsafe {std::mem::zeroed()},
+            backend_new_input_listener: unsafe {std::mem::zeroed()},
             xdg_shell_new_surface_listener: unsafe {std::mem::zeroed()},
+            cursor_motion_listener: unsafe {std::mem::zeroed()},
+            cursor_motion_absolute_listener: unsafe {std::mem::zeroed()},
+            cursor_button_listener: unsafe {std::mem::zeroed()},
+            cursor_axis_listener: unsafe {std::mem::zeroed()},
+            cursor_frame_listener: unsafe {std::mem::zeroed()},
+            seat_request_set_cursor_listener: unsafe {std::mem::zeroed()},
         });
         unsafe {
             let ctx = c.as_mut().get_unchecked_mut();
@@ -57,6 +72,7 @@ impl Server {
             ctx.output_layout = wlr_output_layout_create();
 
             connect_listener!(ctx, backend, new_output);
+            connect_listener!(ctx, backend, new_input);
 
             ctx.xdg_shell = wlr_xdg_shell_create(ctx.display);
 
@@ -66,7 +82,18 @@ impl Server {
             wlr_cursor_attach_output_layout(ctx.cursor, ctx.output_layout);
             ctx.cursor_mgr = wlr_xcursor_manager_create(std::ptr::null(), 24);
             wlr_xcursor_manager_load(ctx.cursor_mgr, 1.0);
+
+
+            connect_listener!(ctx, cursor, motion);
+            connect_listener!(ctx, cursor, motion_absolute);
+            connect_listener!(ctx, cursor, button);
+            connect_listener!(ctx, cursor, axis);
+            connect_listener!(ctx, cursor, frame);
+
             ctx.seat = wlr_seat_create(ctx.display, b"seat0\0".as_ptr() as *const _);
+
+            connect_listener!(ctx, seat, request_set_cursor);
+
             let socket_name_ptr = ffi_dispatch!(WAYLAND_SERVER_HANDLE, wl_display_add_socket_auto, ctx.display as *mut _);
             if socket_name_ptr.is_null() {
                 return Err("cannot create socket");
@@ -106,13 +133,44 @@ impl std::ops::Drop for Server {
 }
 
 implement_listener!(Server, backend, new_output, wlr_output);
+implement_listener!(Server, backend, new_input, wlr_input_device);
 implement_listener!(Server, xdg_shell, new_surface, wlr_xdg_surface);
+implement_listener!(Server, cursor, motion, wlr_event_pointer_motion);
+implement_listener!(Server, cursor, motion_absolute, wlr_event_pointer_motion_absolute);
+implement_listener!(Server, cursor, button, wlr_event_pointer_button);
+implement_listener!(Server, cursor, axis, wlr_event_pointer_axis);
+implement_listener!(Server, cursor, frame, libc::c_void);
+implement_listener!(Server, seat, request_set_cursor, wlr_seat_pointer_request_set_cursor_event);
 impl Server {
     fn backend_new_output(self: Pin<&mut Self>, output: *mut wlr_output) {
         println!("new output!");
     }
+    fn backend_new_input(self: Pin<&mut Self>, input: *mut wlr_input_device) {
+        unsafe {
+            let ctx = self.get_unchecked_mut();
+            wlr_cursor_attach_input_device(ctx.cursor, input);
+        }
+    }
     fn xdg_shell_new_surface(self: Pin<&mut Self>, surface: *mut wlr_xdg_surface) {
         println!("new xdg surface!");
+    }
+    fn cursor_motion(self: Pin<&mut Self>, event: *mut wlr_event_pointer_motion) {
+        println!("cursor motion!");
+    }
+    fn cursor_motion_absolute(self: Pin<&mut Self>, event: *mut wlr_event_pointer_motion_absolute) {
+        println!("cursor motion absolute!");
+    }
+    fn cursor_button(self: Pin<&mut Self>, event: *mut wlr_event_pointer_button) {
+        println!("cursor button!");
+    }
+    fn cursor_axis(self: Pin<&mut Self>, event: *mut wlr_event_pointer_axis) {
+        println!("cursor axis!");
+    }
+    fn cursor_frame(self: Pin<&mut Self>, event: *mut libc::c_void) {
+        println!("cursor motion!");
+    }
+    fn seat_request_set_cursor(self: Pin<&mut Self>, event: *mut wlr_seat_pointer_request_set_cursor_event) {
+        println!("request set cursor!");
     }
 }
 
